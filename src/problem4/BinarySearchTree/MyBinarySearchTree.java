c93a530cf0c8987a360b6096bae0a4e11ea3c3f7
pulkit2012 <pulkit.varshney_iot18@gla.ac.in>
2020-03-28 20:16:27 +0530

    problem 4 completed

---
+++ src/problem4/BinarySearchTree/MyBinarySearchTree.java
@@ -0,0 +1,98 @@
+package problem4.BinarySearchTree;
+import problem4.Node.MyNode;
+import problem4.myqueue.MyQueue;
+public class MyBinarySearchTree {
+    public problem1.node.TreeNode root;
+    private MyQueue myqueue;
+    public problem1.node.TreeNode getRoot(){
+        return root;
+    }
+
+    public MyBinarySearchTree(MyQueue myqueue) {
+        this.myqueue = myqueue;
+    }
+
+    public problem4.BinarySearchTree.MyBinarySearchTree insert(int value) {
+        problem1.node.TreeNode node = new problem1.node.TreeNode(value);
+        if (root == null) {
+            root = node;
+            return this;
+        }
+        insertRec(root, node);
+        return this;
+    }
+
+    private void insertRec(problem1.node.TreeNode latestRoot, problem1.node.TreeNode node) {
+        if (latestRoot.value > node.value) {
+            if (latestRoot.left == null) {
+                latestRoot.left = node;
+                return;
+            } else {
+                insertRec(latestRoot.left, node);
+            }
+        } else {
+            if (latestRoot.right == null) {
+                latestRoot.right = node;
+                return;
+            } else {
+                insertRec(latestRoot.right, node);
+            }
+        }
+    }
+
+    public void printInorder() {
+        printInOrderRec(root);
+        System.out.println("");
+    }
+
+    private void printInOrderRec(problem1.node.TreeNode currRoot) {
+
+        if (currRoot == null) {
+            return;
+        }
+        printInOrderRec(currRoot.left);
+        System.out.print(currRoot.value + "  ");
+        printInOrderRec(currRoot.right);
+    }
+
+    public void printPreorder() {
+        printPreOrderRec(root);
+        System.out.println(" ");
+    }
+
+    private void printPreOrderRec(problem1.node.TreeNode currRoot) {
+        if (currRoot == null) {
+            return;
+        }
+        myqueue.enqueue(currRoot.value);
+        printPreOrderRec(currRoot.left);
+        printPreOrderRec(currRoot.right);
+    }
+
+    public void printPostorder() {
+        printPostOrderRec(root);
+        System.out.println("");
+    }
+
+    private void printPostOrderRec(problem1.node.TreeNode currRoot) {
+        if (currRoot == null) {
+            return;
+        }
+        printPostOrderRec(currRoot.left);
+        printPostOrderRec(currRoot.right);
+        System.out.print(currRoot.value + "  ");
+    }
+
+    private int countrr = 0;
+
+    public int getCountrr() {
+        return countrr;
+    }
+
+    public void setCountrr(int countrr) {
+        this.countrr = countrr;
+    }
+}
+
+
+
