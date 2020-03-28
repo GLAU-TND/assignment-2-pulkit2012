93aefa70340c016edbb7c29e523175d98cd87bea
pulkit2012 <pulkit.varshney_iot18@gla.ac.in>
2020-03-28 20:14:21 +0530

    problem 2 completed

---
+++ src/problem2/main/BinarySearchTree.java
@@ -0,0 +1,112 @@
+package problem2.main;
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+public class BinarySearchTree {
+    public TreeNode root;
+    private MyQueue preQueue;
+    private MyQueue postQueue;
+
+    public BinarySearchTree(MyQueue preQueue, MyQueue postQueue) {
+        this.preQueue = preQueue;
+        this.postQueue = postQueue;
+    }
+
+    public MyQueue getPreQueue() {
+        return preQueue;
+    }
+
+    public void setPreQueue(MyQueue preQueue) {
+        this.preQueue = preQueue;
+    }
+
+    public MyQueue getPostQueue() {
+        return postQueue;
+    }
+
+    public void setPostQueue(MyQueue postQueue) {
+        this.postQueue = postQueue;
+    }
+
+    public problem2.main.BinarySearchTree insert(int value) {
+        TreeNode node = new TreeNode(value);
+        if (root == null) {
+            root = node;
+            return this;
+        }
+        insertRec(root, node);
+        return this;
+    }
+
+    private void insertRec(TreeNode latestRoot, TreeNode node) {
+        if (latestRoot.value > node.value) {
+            if (latestRoot.left == null) {
+                latestRoot.left = node;
+                return;
+            } else {
+                insertRec(latestRoot.left, node);
+            }
+        } else {
+            if (latestRoot.right == null) {
+                latestRoot.right = node;
+                return;
+            } else {
+                insertRec(latestRoot.right, node);
+            }
+        }
+    }
+
+    public void printInorder() {
+        printInOrderRec(root);
+        System.out.println("");
+    }
+
+    private void printInOrderRec(TreeNode currRoot) {
+
+        if (currRoot == null) {
+            return;
+        }
+        printInOrderRec(currRoot.left);
+        System.out.print(currRoot.value + "  ");
+        printInOrderRec(currRoot.right);
+    }
+
+    public void printPreorder() {
+        printPreOrderRec(root);
+        System.out.println("");
+    }
+
+    private void printPreOrderRec(TreeNode currRoot) {
+        if (currRoot == null) {
+            return;
+        }
+        preQueue.enqueue(currRoot.value);
+        printPreOrderRec(currRoot.left);
+        printPreOrderRec(currRoot.right);
+    }
+
+    public void printPostorder() {
+        printPostOrderRec(root);
+        System.out.println("");
+    }
+
+    private void printPostOrderRec(TreeNode currRoot) {
+        if (currRoot == null) {
+            return;
+        }
+        printPostOrderRec(currRoot.left);
+        printPostOrderRec(currRoot.right);
+        postQueue.enqueue(currRoot.value);
+    }
+
+    private int countrr = 0;
+
+    public int getCountrr() {
+        return countrr;
+    }
+
+    public void setCountrr(int countrr) {
+        this.countrr = countrr;
+    }
+
+}
+
--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+import problem2.main.BinarySearchTree;
+import problem4.myqueue.MyQueue;
+import problem2.main.BinarySearchTree;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue preQueue = new MyQueue();
+        MyQueue postQueue = new MyQueue();
+        BinarySearchTree binarySearchTree =  new BinarySearchTree(preQueue,postQueue);
+        binarySearchTree.insert(19);
+        binarySearchTree.insert(12);
+        binarySearchTree.insert(8);
+        binarySearchTree.insert(20);
+        binarySearchTree.printPreorder();
+        binarySearchTree.printPostorder();
+        System.out.println("PreOrder: ");
+        preQueue.show();
+        System.out.println("PostOrder: ");
+        postQueue.show();
+    }
 }
